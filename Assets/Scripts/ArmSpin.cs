cc8c70b8df4cd99ddd02411612c3fdce50e116fc
Jesse Moore <jessem@mtu.edu>
2016-09-10 13:06:25 -0400

    Character Sprites

---
+++ Assets/Scripts/ArmSpin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmSpin : MonoBehaviour {
+
+    RotateTowardsInput rotateScript;
+    Vector3 pointOfRotation;
+    float armLength;
+
+	// Use this for initialization
+	void Start () {
+        rotateScript = GetComponent<RotateTowardsInput>();
+        pointOfRotation = transform.parent.position;
+        armLength = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateScript.inputVector = rotateScript.mousePos - pointOfRotation;
+        repositionArm();
+        rotateScript.rotate();
+    }
+
+    void repositionArm()
+    {
+        Vector3 vec = rotateScript.inputVector.normalized;
+        transform.localPosition = vec * armLength;
+    }
+}
---
+++ Assets/Scripts/RotateTowardsInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTowardsInput : MonoBehaviour {
+
+    public Vector3 inputVector;
+    public Vector3 mousePos;
+
+	// Use this for initialization
+	void Start () {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        inputVector = mousePos - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        inputVector = mousePos - transform.position;
+        rotate();
+	}
+
+    public void rotate()
+    {
+        float angle = Mathf.Atan2(inputVector.y, inputVector.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        if (Mathf.Abs(angle) > 90f)
+        {
+            transform.localScale = new Vector2(transform.localScale.x, -Mathf.Abs(transform.localScale.y));
+        }
+        else
+        {
+            transform.localScale = new Vector2(transform.localScale.x, Mathf.Abs(transform.localScale.y));
+        }
+    }
+}
