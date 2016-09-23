27f2f97f27ad7d45eee6bfe6cf62ec36bd05bd51
Ronald Liu <ronaldl@mtu.edu>
2016-09-22 21:31:25 -0400

    Implementation of a TileMap using Lean and LitJson

--- Assets/Scripts/TileMap_Scripts/TileMapController.cs
+++ Assets/Scripts/TileMap_Scripts/TileMapController.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.IO;
 using Lean;
-
-// An implementation of a TileMap provided by:
-// 	https://gamedevacademy.org/how-to-script-a-2d-tile-map-in-unity3d/
+using LitJson;
 
 public enum Tiles {
+	// TODO: Update these...
 	Unset,
 	Ground,
 	Spike,
@@ -42,13 +42,25 @@ public class TileMapController : MonoBehaviour {
 	public Sprite defaultImage;
 	public GameObject tileContainerPrefab;
 	public GameObject tilePrefab;
-	public Vector2 currentPosition;
-	public Vector2 viewPortSize;
+	public GameObject toFollow;
 
+	private Vector2 currentPosition;
 	private TileSprite[,] _map;
 	private GameObject controller;
 	private GameObject _tileContainer;
 	private List<GameObject> _tiles = new List<GameObject>();
+
+	private string jsonString;
+	private JsonData objJson;
+
+	private JsonData generateJsonObject (string fname)
+	{
+		jsonString = File.ReadAllText(Application.dataPath + "/JSON_files/" + fname);
+		Debug.Log (jsonString);
+		objJson = JsonMapper.ToObject (jsonString);
+
+		return objJson;
+	}
 
 	private TileSprite findTile(Tiles tile)
 	{
@@ -59,6 +71,9 @@ public class TileMapController : MonoBehaviour {
 		return null;
 	}
 
+	// Used to create default tiles
+	// Not used currently cause I only want tiles that the map
+	//	wants has specified to spawn
 	private void defaultTiles() 
 	{
 		for (int y = 0; y < mapSize.y; y++) {
@@ -68,15 +83,15 @@ public class TileMapController : MonoBehaviour {
 		}
 	}
 
-	private void setTiles()
+	private void setTiles(JsonData map)
 	{
 		int index = 0;
 		for (int y = 0; y < mapSize.y; y++) {
 			for (int x = 0; x < mapSize.x; x++) {
-				_map [x, y] = new TileSprite (tileSprites[index].tName, tileSprites[index].tImage, tileSprites[index].tType);
+				int tileID = (int) map ["layers"] [0] ["data"] [index];
+				if (tileID != 0)
+					_map [x, (int) mapSize.y - y] = new TileSprite (tileSprites[tileID].tName, tileSprites[tileID].tImage, tileSprites[tileID].tType);
 				index++;
-				if (index > tileSprites.Count - 1)
-					index = 0;
 			}
 		}
 	}
@@ -89,17 +104,43 @@ public class TileMapController : MonoBehaviour {
 		_tiles.Clear ();
 		LeanPool.Despawn(_tileContainer);
 		_tileContainer = LeanPool.Spawn (tileContainerPrefab);
+
+		for (float y = 0; y < mapSize.y; y++) {
+			for (float x = 0; x < mapSize.x; x++) {
+				if (_map [(int)x, (int)y] == null)
+					continue;
+				GameObject t = LeanPool.Spawn (tilePrefab);
+				t.transform.position = new Vector3 (x, y, -8);
+				t.transform.SetParent (_tileContainer.transform);
+				SpriteRenderer renderer = t.GetComponent<SpriteRenderer> ();
+				renderer.sprite = _map [(int)x , (int)y].tImage;
+				_tiles.Add (t);
+			}
+		}
+
+		// If you want to procedurally generate the tilemap as the character moves
+		// Kind of weird and doesnt seem smooth
+		// So taking it out for now... Maybe we might need in the future?
+		/*
+		foreach (GameObject o in _tiles) {
+			LeanPool.Despawn (o);
+		}
+		_tiles.Clear ();
+		LeanPool.Despawn(_tileContainer);
+		_tileContainer = LeanPool.Spawn (tileContainerPrefab);
 		float tileSize = 0.64F;
 		float viewOffsetX = viewPortSize.x / 2F;
 		float viewOffsetY = viewPortSize.y / 2F;
+		
 		for (float y = -viewOffsetY; y < viewOffsetY; y++) {
 			for (float x = -viewOffsetX; x < viewOffsetX; x++) {
-				float tX = x*tileSize;
-				float tY = y*tileSize;
+				float tX = x+tileSize;
+				float tY = y+tileSize;
 
 				float iX = x + currentPosition.x;
 				float iY = y + currentPosition.y;
 
+				// Make sure that we do not go out of bounds
 				if (iX < 0)
 					continue;
 				if (iY < 0)
@@ -110,28 +151,31 @@ public class TileMapController : MonoBehaviour {
 					continue;
 				
 				GameObject t = LeanPool.Spawn (tilePrefab);
-				t.transform.position = new Vector3 (tX, tY, -9);
+				t.transform.position = new Vector3 (tX, tY, -8);
 				t.transform.SetParent (_tileContainer.transform);
 				SpriteRenderer renderer = t.GetComponent<SpriteRenderer> ();
 				renderer.sprite = _map [(int)x + (int)currentPosition.x, (int)y + (int)currentPosition.y].tImage;
 				_tiles.Add (t);
 			}
 		}
+		*/
 	}
 
 	public void Start()
 	{
-		controller = GameObject.Find ("TileMapController");
+		// controller = GameObject.Find ("TileMapController");
+		// Camera cam = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		// float height = 2f * cam.orthographicSize;
+		// float width = height * cam.aspect;
+
 		_map = new TileSprite[(int) mapSize.x, (int) mapSize.y];
 
-		defaultTiles ();
-		setTiles ();
+		JsonData map = generateJsonObject ("test_2.json");
+
+		setTiles (map);
+		addTilesToWorld ();
 	}
 
 	private void Update()
-	{
-		// We probably do not need to keep this in the Update cause
-		//	we only need to load the map once
-		addTilesToWorld ();
-	}
+	{}
 }
