b0718d88e8a3595f6a0d194118365fde5a21a24f
Michael Gryzwa <magryzwa@mtu.edu>
2016-09-24 12:41:30 -0400

    Added Sentry Prefab

---
+++ Assets/Scripts/SentryController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SentryController : MonoBehaviour {
+
+	public Transform Target;
+	public float RotateSpeed;
+	private Quaternion lookRotation;
+	private Vector3 direction;
+
+	void Update(){
+		direction = (Target.position - transform.position).normalized;
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		lookRotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime * RotateSpeed);
+	}
+
+
+}
