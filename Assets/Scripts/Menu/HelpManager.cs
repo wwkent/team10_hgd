60cad4fdbefe7bc7d3cd30277073699b4e8a77ac
Andrew Fox III <avfox@mtu.edu>
2017-01-22 13:49:58 -0500

    menu almost finished

---
+++ Assets/Scripts/Menu/HelpManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpManager : MonoBehaviour {
+	public Transform optionsMenu;
+	public UnityEngine.EventSystems.EventSystem events;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+	}
+
+	public void ControlsMenu(bool clicked){
+		//switch controls for help or controls menu
+		optionsMenu.FindChild("Help Text").gameObject.SetActive(!clicked);
+		optionsMenu.FindChild("Help").gameObject.SetActive(clicked);
+		optionsMenu.FindChild("Controls Text").gameObject.SetActive(clicked);
+		optionsMenu.FindChild("Controls").gameObject.SetActive(!clicked);
+		//change some other stuff
+	}
+}
